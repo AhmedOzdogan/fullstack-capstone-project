249d1eea503b108730db1af12101988f112dee3d
AhmedOzdogan <98954345+AhmedOzdogan@users.noreply.github.com>
2025-10-10 10:38:06 +0700

    CI errors fixed

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -5,7 +5,6 @@ const cors = require("cors");
 const pinoLogger = require("./logger");
 
 const connectToDatabase = require("./models/db");
-const { loadData } = require("./util/import-mongo/index");
 
 const app = express();
 app.use("*", cors());
@@ -35,7 +34,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/search", searchRoutes);
 
 // Global Error Handler
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   console.error(err);
   res.status(500).send("Internal Server Error");
 });
